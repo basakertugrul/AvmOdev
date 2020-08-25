eba7e517b1345e368e25032792a2bf56884c5d23
basakertugrul <61563492+basakertugrul@users.noreply.github.com>
2020-08-25 14:17:11 +0300

    aciklama

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './clb.dart';
 import './avm.dart';
+import 'package:multiselect_formfield/multiselect_formfield.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,79 +10,130 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
-Class MyHomePage extends StatelessWidget {
-  final Avm avm = new Avm();
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Avm avm = new Avm(
+      ad: null,
+      fenerium: null,
+      burgerKing: null,
+      sinema: null,
+      koton: null,
+      mavi: null,
+      mango: null,
+      migros: null);
+  List _myActivities;
+  String _myActivitiesResult;
+  final formKey = new GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _myActivities = [];
+    _myActivitiesResult = '';
+  }
+
+  _saveForm() {
+    var form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      setState(() {
+        _myActivitiesResult = _myActivities.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(80, 77, 229, 1),
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text("CHART!"),
-              elevation: 5,
-              color: Colors.blue,
-            ),
-          ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: "Title"),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: "Amount"),
-                    ),
-                    FlatButton(
-                      child: Text("Add Transactions"),
-                      textColor: Colors.purple,
-                      onPressed: () {},
-                    )
-                  ]),
-            ),
-          ),
-          Column(
-           children: avm.brandList.map((tx) {
-              return Card(
-                  child: Row(children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '?',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16),
+                child: MultiSelectFormField(
+                  autovalidate: false,
+                  titleText: 'My workouts',
+                  validator: (value) {
+                    if (value == null || value.length == 0) {
+                      return 'Please select one or more options';
+                    }
+                    return null;
+                  },
+                  dataSource: [
+                    {
+                      "display": "Running",
+                      "value": "Running",
+                    },
+                    {
+                      "display": "Climbing",
+                      "value": "Climbing",
+                    },
+                    {
+                      "display": "Walking",
+                      "value": "Walking",
+                    },
+                    {
+                      "display": "Swimming",
+                      "value": "Swimming",
+                    },
+                    {
+                      "display": "Soccer Practice",
+                      "value": "Soccer Practice",
+                    },
+                    {
+                      "display": "Baseball Practice",
+                      "value": "Baseball Practice",
+                    },
+                    {
+                      "display": "Football Practice",
+                      "value": "Football Practice",
+                    },
+                  ],
+                  textField: 'display',
+                  valueField: 'value',
+                  okButtonLabel: 'OK',
+                  cancelButtonLabel: 'CANCEL',
+                  // required: true,
+                  hintText: 'Please choose one or more',
+                  initialValue: _myActivities,
+                  onSaved: (value) {
+                    if (value == null) return;
+                    setState(() {
+                      _myActivities = value;
+                    });
+                  },
                 ),
-                Text("tx")
-              ]));
-            }).toList(),
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: RaisedButton(
+                  child: Text('Save'),
+                  onPressed: _saveForm,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Text(_myActivitiesResult),
+              )
+            ],
           ),
-          Card(
-            child: Container(
-              height: 30,
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
